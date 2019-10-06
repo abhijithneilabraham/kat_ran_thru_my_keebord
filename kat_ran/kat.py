834b41eb7e658ab4e3bfe1c1a490b47795447271
Tom Johnson V <tomv.johnson2100@gmail.com>
2019-10-06 22:45:37 +0530

    Update kat.py
    meowing is inevitable

--- kat_ran/kat.py
+++ kat_ran/kat.py
@@ -11,13 +11,13 @@ c=int(input('write in number how much you love a cat \n'))
 def catran():
     a=random.randint(97,122)
      if a>=97 and a<=103:
-        playsound('path\of the\directory\cat1.mp3') #cat1.mp3, cat2.mp3, cat3.mp3 and cat4.mp3 is provided in the repo
+        playsound('path\of\the\directory\cat1.mp3') #cat1.mp3, cat2.mp3, cat3.mp3 and cat4.mp3 is provided in the repo
     if a>103 and a<=110:
-        playsound('path\of the\directory\cat2.mp3')
+        playsound('path\of\the\directory\cat2.mp3')
     if a>110 and a<=117:
-        playsound('path\of the\directory\cat3.mp3')
+        playsound('path\of\the\directory\cat3.mp3')
     if a>117 and a<122:
-        playsound('path\of the\directory\cat4.mp3')
+        playsound('path\of\the\directory\cat4.mp3')
     print(chr(a),end="")
 r=0.05
 for i in range(c):
