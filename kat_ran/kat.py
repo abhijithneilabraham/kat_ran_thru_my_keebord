bc2e10775fa3dd49f2e748911f4961afaf9ae023
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-10-07 22:48:39 +0530

    local changes

--- kat_ran/kat.py
+++ kat_ran/kat.py
@@ -10,14 +10,14 @@ import time
 c=int(input('write in number how much you love a cat \n'))    
 def catran():
     a=random.randint(97,122)
-     if a>=97 and a<=103:
-        playsound('path\of\the\directory\cat1.mp3') #cat1.mp3, cat2.mp3, cat3.mp3 and cat4.mp3 is provided in the repo
+    if a>=97 and a<=103:
+        playsound('cataudios/cat1.mp3') #cat1.mp3, cat2.mp3, cat3.mp3 and cat4.mp3 is provided in the repo
     if a>103 and a<=110:
-        playsound('path\of\the\directory\cat2.mp3')
+        playsound('cataudios/cat2.mp3')
     if a>110 and a<=117:
-        playsound('path\of\the\directory\cat3.mp3')
+        playsound('cataudios/cat3.mp3')
     if a>117 and a<122:
-        playsound('path\of\the\directory\cat4.mp3')
+        playsound('cataudios/cat4.mp3')
     print(chr(a),end="")
 r=0.05
 for i in range(c):
