634f5458d173a4a177dad5442ef1f7f736f4d637
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-10-12 22:46:48 +0530

    Update kat_ran/kat.py

--- kat_ran/kat.py
+++ kat_ran/kat.py
@@ -8,22 +8,23 @@ Created on Fri Oct  4 23:12:15 2019
 """
 import random
 import time
-#from playsound import playsound
+from playsound import playsound
 
-#kat_sounds = ["cat1.mp3", "cat2.mp3", "cat3.mp3", "cat4.mp3", "cat5.mp3", "cat6.mp3"]
+kat_sounds = ["cat1.mp3", "cat2.mp3", "cat3.mp3", "cat4.mp3", "cat5.mp3", "cat6.mp3"]
+
+
 c = int(input('Write in number how much you love a cat \n'))    
-def catran():
 
 
-    a = random.randint(97, 136)
-    print(chr(a), end="")
 
 r = 0.05
 for i in range(c):
-    if i % 5 == 0:
+    if i % 5 == 0 and i !=0:
         r = random.uniform(0.01,0.2)
+        playsound("cataudios/" + random.choice(kat_sounds))
+    a = random.randint(97, 136)
+    print(chr(a), end="")        
     time.sleep(r)
-    catran()
 
 print("\n Oops, cat ran", str(c), "times through ya keyboard!")
 
