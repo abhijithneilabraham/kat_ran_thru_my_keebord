245d0a66e07b15bc51c27520ae3bfeacf138f876
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-10-12 22:30:16 +0530

    dont make mistakes

--- kat_ran/kat.py
+++ kat_ran/kat.py
@@ -13,7 +13,7 @@ from playsound import playsound
 kat_sounds = ["cat1.mp3", "cat2.mp3", "cat3.mp3", "cat4.mp3", "cat5.mp3", "cat6.mp3"]
 c = int(input('Write in number how much you love a cat \n'))    
 def catran():
-    playsound("catsounds/" + random.choice(kat_sounds))
+#    playsound("cataudios/" + random.choice(kat_sounds))
     a = random.randint(97, 136)
     print(chr(a), end="")
 
@@ -26,3 +26,24 @@ for i in range(c):
 
 print("\n Oops, cat ran", str(c), "times through ya keyboard!")
 
+
+from tkinter import *
+
+root = Tk()
+imagelist = ["kat.jpeg"]
+# extract width and height info
+photo = PhotoImage(file=imagelist[0])
+width = photo.width()
+height = photo.height()
+canvas = Canvas(width=width, height=height)
+canvas.pack()
+# create a list of image objects
+giflist = []
+for imagefile in imagelist:
+    photo = PhotoImage(file=imagefile)
+    giflist.append(photo)
+# loop through the gif image objects for a while
+canvas.create_image(width/2.0, height/2.0, image=giflist[0])
+canvas.update()
+time.sleep(0.1)
+root.mainloop()
