1e0fb87e9ec0d22268a1fd8556fe605be56fb764
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-10-12 23:09:58 +0530

    Update kat_ran/kat.py

--- kat_ran/kat.py
+++ kat_ran/kat.py
@@ -9,22 +9,34 @@ Created on Fri Oct  4 23:12:15 2019
 import random
 import time
 from playsound import playsound
+def katran():
+    
+    kat_sounds = ["cat1.mp3", "cat2.mp3", "cat3.mp3", "cat4.mp3", "cat5.mp3", "cat6.mp3"]
+    
+    print('cat is running through ya keyboard.Press ctrl+c two times to exit')
+      
+    
+    
+    
+    r = 0.05
+    i=1
+    while i%2!=0:
+        try:
+            if i % 5 == 0:
+                r = random.uniform(0.01,0.2)
+                playsound("cataudios/" + random.choice(kat_sounds))
+            a = random.randint(97, 136)
+            print(chr(a), end="")        
+            time.sleep(r)
+    
+            i+=2        
+        except KeyboardInterrupt:
+            print("\n Oops, cat ran through ya keyboard!")
+            break
+        
+            
+        
 
-kat_sounds = ["cat1.mp3", "cat2.mp3", "cat3.mp3", "cat4.mp3", "cat5.mp3", "cat6.mp3"]
-
-
-c = int(input('Write in number how much you love a cat \n'))    
-
-
-
-r = 0.05
-for i in range(c):
-    if i % 5 == 0 and i !=0:
-        r = random.uniform(0.01,0.2)
-        playsound("cataudios/" + random.choice(kat_sounds))
-    a = random.randint(97, 136)
-    print(chr(a), end="")        
-    time.sleep(r)
-
-print("\n Oops, cat ran", str(c), "times through ya keyboard!")
-
+katran()
+if input()=='q':
+    exit()
